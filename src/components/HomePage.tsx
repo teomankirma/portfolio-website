0f48b5acda323b77f13166ec3b0523e8781a6627
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2025-03-21 13:34:22 -0700

    feat: language setup, home page styling improvements

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,33 +4,41 @@ import React from "react";
 import { Spacer } from "@/components";
 import { Avatar, AvatarFallback, AvatarImage, Button } from "@/components/ui";
 import { Typewriter } from "react-simple-typewriter";
+import { CONSTANTS } from "@/constants/constants";
+import { usePortfolioStore } from "@/hooks/usePortfolioStore";
 
 export function HomePage() {
+  const { language } = usePortfolioStore();
+
   return (
     <div className="flex min-h-screen justify-center" id="home">
       <div className="flex flex-col items-center w-full">
         <Avatar className="h-60 w-60 mt-20">
           <AvatarImage
             src="https://avatars.githubusercontent.com/u/83593653?v=4"
-            alt="Teoman Kirma"
+            alt={CONSTANTS[language].fullName}
           />
-          <AvatarFallback>Teoman Kirma</AvatarFallback>
+          <AvatarFallback>{CONSTANTS[language].fullName}</AvatarFallback>
         </Avatar>
         <Spacer y={4} />
-        <h1 className="text-3xl font-bold">Welcome</h1>
+        <h1 className="text-4xl font-bold">{CONSTANTS[language].welcome}</h1>
         <Spacer y={2} />
         <div className="text-center min-w-[200px] text-3xl font-bold">
           <Typewriter
-            words={["Hello", "this", "is", "a"]}
+            words={CONSTANTS[language].typewriter}
             loop={0}
             cursor={true}
             cursorColor="#17c964"
           />
         </div>
         <Spacer y={2} />
-        <h1 className="text-2xl font-bold">based in Los Angeles, US.</h1>
-        <Spacer y={2} />
-        <Button size={"lg"}>Hire me!</Button>
+        <h1 className="text-2xl font-bold">{CONSTANTS[language].location}</h1>
+        <Spacer y={3} />
+        <Button size={"lg"} className="bg-green-500 p-6 rounded-3xl">
+          <span className="text-lg font-bold">
+            {CONSTANTS[language].hireMe}
+          </span>
+        </Button>
       </div>
     </div>
   );
