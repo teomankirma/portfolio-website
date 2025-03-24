7d69149d594b692a9b757c813ea3a22136743f65
Teoman Kırma <83593653+teomankirma@users.noreply.github.com>
2025-03-24 07:52:09 -0700

    feat: tailwind-prettier config and final adjustments to home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,14 +6,15 @@ import { Avatar, AvatarFallback, AvatarImage, Button } from "@/components/ui";
 import { Typewriter } from "react-simple-typewriter";
 import { CONSTANTS } from "@/constants/constants";
 import { usePortfolioStore } from "@/hooks/usePortfolioStore";
+import { scrollToSection } from "@/utils";
 
 export function HomePage() {
   const { language } = usePortfolioStore();
 
   return (
     <div className="flex min-h-screen justify-center" id="home">
-      <div className="flex flex-col items-center w-full">
-        <Avatar className="h-60 w-60 mt-20">
+      <div className="flex w-full flex-col items-center">
+        <Avatar className="mt-20 h-60 w-60">
           <AvatarImage
             src="https://avatars.githubusercontent.com/u/83593653?v=4"
             alt={CONSTANTS[language].fullName}
@@ -23,7 +24,7 @@ export function HomePage() {
         <Spacer y={4} />
         <h1 className="text-4xl font-bold">{CONSTANTS[language].welcome}</h1>
         <Spacer y={2} />
-        <div className="text-center min-w-[200px] text-3xl font-bold">
+        <div className="min-w-[200px] text-center text-3xl font-bold">
           <Typewriter
             words={CONSTANTS[language].typewriter}
             loop={0}
@@ -34,10 +35,16 @@ export function HomePage() {
         <Spacer y={2} />
         <h1 className="text-2xl font-bold">{CONSTANTS[language].location}</h1>
         <Spacer y={3} />
-        <Button size={"lg"} className="bg-green-500 p-6 rounded-3xl">
-          <span className="text-lg font-bold">
-            {CONSTANTS[language].hireMe}
-          </span>
+        <Button
+          size={"lg"}
+          variant="outline"
+          className="rounded-3xl bg-green-500 p-6 text-white hover:border-green-500 hover:bg-transparent dark:bg-green-500 dark:hover:border-green-500 dark:hover:bg-transparent"
+          onClick={e => {
+            // @ts-expect-error ignore event type mismatch
+            scrollToSection(e, "#get-in-touch");
+          }}
+        >
+          <span className="text-lg font-semibold">{CONSTANTS[language].hireMe}</span>
         </Button>
       </div>
     </div>
